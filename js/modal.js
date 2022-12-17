c5910605004fef7ef1b18b629f49622ed4444ec7
Nick-Perelygin <77kalian77@gmail.com>
2022-12-18 00:44:11 +0200

    22

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,10 @@
-(() => {
-  const refs = {
+const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+    modal: document.querySelector("[data-modal]"),};
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");}
